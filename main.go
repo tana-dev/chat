76b5143d5464f54e4e84b430fc0d7805f6861ad9
tana-dev <nekodanshaku27@gmail.com>
2019-02-10 00:14:15 +0900

    initial commit

---
+++ main.go
@@ -0,0 +1,45 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"path/filepath"
+	"sync"
+	"text/template"
+)
+
+type templateHandler struct {
+	once     sync.Once
+	filename string
+	templ    *template.Template
+}
+
+func (t *templateHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	t.once.Do(func() {
+		t.templ = template.Must(template.ParseFiles(filepath.Join("templates", t.filename)))
+	})
+	ttt := t.templ.Execute(w, nil)
+	fmt.Println(ttt)
+}
+
+func main() {
+	//	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+	//		w.Write([]byte(`
+	//		<html>
+	//			<head>
+	//				<title>tanaka</title>
+	//			</head>
+	//			<body>
+	//			</body>
+	//		</html>
+	//		`))
+	//	})
+
+	http.Handle("/", &templateHandler{filename: "main.html"})
+
+	if err := http.ListenAndServe(":8080", nil); err != nil {
+		log.Fatal("ListenAndServe", err)
+	}
+
+}
